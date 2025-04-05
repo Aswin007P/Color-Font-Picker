f2d884db24cb066e57ac1d58b145efb2fb48537d
Aswin <suryanaryanpattanayak@gmail.com>
2025-04-05 10:32:11 +0530

    Updated Project

--- Scrip.js
+++ Scrip.js
@@ -9,6 +9,7 @@
             h2.style.color=color;
         }
         function changeBackGroundColor(bgcolor){
+            h2.style.backgroundImage = "none";
             h2.style.backgroundColor=bgcolor;
         }
         function changeFont(font){
@@ -29,4 +30,33 @@
         }
         function chngc(){
             h2.style.color=h10.value;
+        }
+        function changeBackGroundImage(src){
+            h2.style.backgroundImage=`url('${src}')`;
+            h2.style.backgroundRepeat = "no-repeat";
+            h2.style.backgroundSize = "cover";       
+            h2.style.backgroundPosition = "center";  
+        }
+        function filed(e){
+            const file = e.files[0];
+            if (!file) return;
+            if (!file.type.startsWith("image/")) {
+                alert("Please select an image file only!");
+                return;
+            }
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                h2.style.background = `url('${e.target.result}') center/cover no-repeat`;
+              };
+      
+              reader.readAsDataURL(file);
+        }
+        function DownloadImage(){
+            html2canvas(h2).then(canvas => {
+                const link = document.createElement("a");
+                link.download = "my-quote.png";
+                link.href = canvas.toDataURL("image/png");
+                link.click();
+            });
+        
         }
